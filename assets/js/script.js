efae17a91ddf75df7c1d677189f1a6c1122c4c62
chloelewisdev <baldwinchloe@hotmail.com>
2020-11-10 14:49:12 +0000

    Added initial js file and js toggle code for cards

---
+++ assets/js/script.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+    // Toggles paragraphs display
+        $("#toggle-btn-1").click(function(){
+        $("#card-1").toggle();
+    });
+        $("#toggle-btn-2").click(function(){
+        $("#card-2").toggle();
+    });
+        $("#toggle-btn-3").click(function(){
+        $("#card-3").toggle();
+    });
+        $("#toggle-btn-4").click(function(){
+        $("#card-4").toggle();
+    });
+});
+
+
+
+
+        ("toggle-btn-1").click(function(){
+        ("#card-1").slideDown()
+    })
