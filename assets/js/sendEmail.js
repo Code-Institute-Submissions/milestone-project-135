b3df0b7d8789f1dd1f17b5777721b0112dacd336
chloelewisdev <baldwinchloe@hotmail.com>
2020-12-02 22:06:38 +0000

    Beautified sendEmail.js code

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -1,23 +1,22 @@
-/*jshint esversion: 8 */ 
-
-function sendMail(contactForm){
+/*jshint esversion: 8 */
+function sendMail(contactForm) {
     emailjs.send("gmail", "newsletter", {
-        "from_firstname": contactForm.firstname.value,
-        "from_lastname": contactForm.lastname.value,
-        "from_email": contactForm.emailaddress.value
-    })
-    .then(
-        function(response) {
-            console.log("SUCCESS", response);
-            $("#myModal").modal("toggle");
-            $("#close-modal").click(function(){
-            location.reload();
-            });
-        },
-        function(error) {
-            console.log("FAILED", error);
-        },
-    );
-    return false;  // To block from loading a new page
+            "from_firstname": contactForm.firstname.value,
+            "from_lastname": contactForm.lastname.value,
+            "from_email": contactForm.emailaddress.value
+        })
+        .then(
+            function(response) {
+                console.log("SUCCESS", response);
+                $("#myModal").modal("toggle");
+                $("#close-modal").click(function() {
+                    location.reload();
+                });
+            },
+            function(error) {
+                console.log("FAILED", error);
+            },
+        );
+    return false; // To block from loading a new page
 }
 
