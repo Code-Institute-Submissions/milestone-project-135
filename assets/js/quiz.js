dd0281ce589a2ae6ef36b7c098e84fa9f29ca7a2
chloelewisdev <baldwinchloe@hotmail.com>
2020-12-02 20:45:13 +0000

    Fourth question added to the Quiz section

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -3,6 +3,7 @@ function check(){
     var question1 = document.quiz.question1.value;
     var question2 = document.quiz.question2.value;
     var question3 = document.quiz.question3.value;
+    var question4 = document.quiz.question4.value;
     var correct = 0;
 
     if (question1 == "1900"){
@@ -16,9 +17,13 @@ function check(){
     if (question3 == "20 million"){
         correct++;
     }
+
+    if (question4 == "Third"){
+        correct++;
+    }
     
     document.getElementById("after_submit").style.visibility = "visible";
-    document.getElementById("number_correct").innerHTML = "You got " + correct + " correct.";
+    document.getElementById("number_correct").innerHTML = "You got " + correct + " correct out of 4.";
 
 }
 
