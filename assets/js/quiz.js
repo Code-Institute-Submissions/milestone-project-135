8eb851fc235210329a3fe5385cde62b19149b04f
chloelewisdev <baldwinchloe@hotmail.com>
2020-12-02 22:03:56 +0000

    Beautified quiz js code

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -1,4 +1,4 @@
-function check(){
+function check() {
 
     var question1 = document.quiz.question1.value;
     var question2 = document.quiz.question2.value;
@@ -6,32 +6,29 @@ function check(){
     var question4 = document.quiz.question4.value;
     var correct = 0;
 
-    if (question1 == "1900"){
-        correct++;
-    }
-    
-    if (question2 == "Girls & Boys"){
-        correct++;
-    }
-    
-    if (question3 == "20 million"){
+    if (question1 == "1900") {
         correct++;
     }
 
-    if (question4 == "Third"){
+    if (question2 == "Girls & Boys") {
         correct++;
     }
-    
+
+    if (question3 == "20 million") {
+        correct++;
+    }
+
+    if (question4 == "Third") {
+        correct++;
+    }
+
     document.getElementById("after_submit").style.visibility = "visible";
     document.getElementById("number_correct").innerHTML = "You got " + correct + " correct out of 4.";
 
 }
 
-    var fullReset = document.getElementById('fullReset');
+var fullReset = document.getElementById('fullReset');
 
-    fullReset.addEventListener('click', function() {
-      location.reload();
-    }, false);
-
-
-
+fullReset.addEventListener('click', function() {
+    location.reload();
+}, false);
