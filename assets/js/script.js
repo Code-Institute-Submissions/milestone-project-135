01fa11f02feca2b749ff269ab1c7afe0a60fc699
chloelewisdev <baldwinchloe@hotmail.com>
2020-12-02 22:09:52 +0000

    Small spacing edits

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,16 @@
 $(document).ready(function() {
 
-    //Add or remove active class on nav 
-
-    $(".nav-link").on("click", function() {
+//Add or remove active class on nav 
+$(".nav-link").on("click", function() {
         $("a.nav-link").removeClass("active");
         $(this).addClass("active");
     });
 });
 
 //Scroll to top button
-
 var mybutton = document.getElementById("myBtn");
 
-// When the user scrolls down 20px from the top of the document, show the button
+// When the user scrolls down 20px from the top of the document, thw button shows
 window.onscroll = function() {
     scrollFunction();
 };
@@ -24,13 +22,14 @@ function scrollFunction() {
         mybutton.style.display = "none";
     }
 }
+
 // When the user clicks on the button, scroll to the top of the document
 function topFunction() {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
 }
 
-// Close mobile dropdown menu after item clicked            
+// Closes mobile dropdown menu after item clicked            
 $('.navbar-nav>li>a').on('click', function() {
     $('.navbar-collapse').collapse('hide');
 });
