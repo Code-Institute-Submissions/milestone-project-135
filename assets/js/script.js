951bc9fba8a7c48e38891c1c879a77901e0cfd35
chloelewisdev <baldwinchloe@hotmail.com>
2020-12-02 22:07:59 +0000

    Beautified code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,33 +1,36 @@
-$(document).ready(function(){
+$(document).ready(function() {
 
-//Add or remove active class on nav 
+    //Add or remove active class on nav 
 
-  $(".nav-link").on("click", function(){
-    $("a.nav-link").removeClass("active");
-    $(this).addClass("active");
-  });
+    $(".nav-link").on("click", function() {
+        $("a.nav-link").removeClass("active");
+        $(this).addClass("active");
+    });
 });
 
 //Scroll to top button
 
-    var mybutton = document.getElementById("myBtn");
+var mybutton = document.getElementById("myBtn");
 
 // When the user scrolls down 20px from the top of the document, show the button
-    window.onscroll = function() {scrollFunction();};
-    function scrollFunction() {
+window.onscroll = function() {
+    scrollFunction();
+};
+
+function scrollFunction() {
     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-            mybutton.style.display = "block";
-        } else {
-            mybutton.style.display = "none";
-        }
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
     }
+}
 // When the user clicks on the button, scroll to the top of the document
-    function topFunction() {
-        document.body.scrollTop = 0;
-        document.documentElement.scrollTop = 0;
-            }
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
 
 // Close mobile dropdown menu after item clicked            
-$('.navbar-nav>li>a').on('click', function(){
+$('.navbar-nav>li>a').on('click', function() {
     $('.navbar-collapse').collapse('hide');
 });
